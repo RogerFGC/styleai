4db38eac4a3b47f415601090a9ebf6414439fc4a
Rogelio <roger.pokemonlive@gmail.com>
2026-02-27 20:37:10 -0600

    fix server

--- server.cjs
+++ server.cjs
@@ -34,9 +34,3 @@ app.post("/analyze", async (req, res) => {
 
 app.listen(process.env.PORT || 3001, () => console.log("Servidor corriendo"));
 ```
-
-Guarda y luego:
-```
-git add .
-git commit -m "fix puerto render"
-git push origin main
